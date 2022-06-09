b00cb09733e63c266fe336b941ff846e38d93914
Sadeghi <sadeghi85@hotmail.com>
2022-06-09 17:20:45 +0430

    devServer config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    client: {
+      webSocketURL: "ws://0.0.0.0:8080/ws",
+    },
+    allowedHosts: "all",
+    /* proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        ws: true,
+        changeOrigin: true,
+      },
+    }, */
+  },
+
   transpileDependencies: true,
 });
