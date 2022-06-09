e65a21ca0e5d8299416c0ba42b63b10fb458ca62
Sadeghi <sadeghi85@hotmail.com>
2022-06-09 19:37:02 +0430

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from "./router";
 
 import PrimeVue from "primevue/config";
 
+import InputText from "primevue/inputtext";
+import Dropdown from "primevue/dropdown";
+
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
@@ -13,5 +16,8 @@ const app = createApp(App);
 app.use(router);
 
 app.use(PrimeVue);
+
+app.component("InputText", InputText);
+app.component("Dropdown", Dropdown);
 
 app.mount("#app");
