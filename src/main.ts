9cc4b116a9d1462efff54f3ec360c2beab3b1d79
Sadeghi <sadeghi85@hotmail.com>
2022-06-09 23:39:40 +0430

    primeflex

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
+import "primeflex/primeflex.css";
+
 const app = createApp(App);
 
 app.use(router);
