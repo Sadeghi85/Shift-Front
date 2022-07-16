c52c1a615429620d7d7f3b4ca6598971cd776f87
Sadeghi <sadeghi85@hotmail.com>
2022-07-17 02:11:15 +0430

    unplugin-auto-import + @typescript-eslint/parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 
   env: {
     node: true,
+    "vue/setup-compiler-macros": true,
   },
 
   extends: [
@@ -16,13 +17,38 @@ module.exports = {
   ],
 
   parser: "vue-eslint-parser",
+
+  // https://github.com/vuejs/eslint-config-typescript/blob/main/index.js
+  plugins: ["@typescript-eslint"],
+  // Prerequisite `eslint-plugin-vue`, being extended, sets
+  // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
+  // in turn delegates to the parser, specified in `parserOptions.parser`:
+  // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: ["./tsconfig.json"],
+    parser: {
+      js: "espree",
+      jsx: "espree",
+
+      ts: require.resolve("@typescript-eslint/parser"),
+      tsx: require.resolve("@typescript-eslint/parser"),
+
+      // Leave the template parser unspecified, so that it could be determined by `<script lang="...">`
+    },
+    extraFileExtensions: [".vue"],
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+        // does not work with type definitions
+        "no-unused-vars": "off",
+      },
+    },
+  ],
 
   // parserOptions: {
   //   ecmaVersion: 2020,
